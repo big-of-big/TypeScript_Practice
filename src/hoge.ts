89f098abeeb47684d20c080e1e6aa91d44726bcd
big-of-big <60137763+big-of-big@users.noreply.github.com>
2021-10-01 17:35:05 +0900

    hoge

--- src/hoge.ts
+++ src/hoge.ts
@@ -1,15 +1,4 @@
 export {}
 
-interface User {
-  id: number
-  name: string
-}
-
-let lamp: User = {
-  id: 1,
-  name: 'lamp',
-}
-
-console.log(lamp)
-
-// interfaceはJSのオブジェクトに独自型を定義するための機能
+const hoge {}
+console.log(hoge.hu);
